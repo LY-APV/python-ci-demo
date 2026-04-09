7c412a022363c4bac99e596c95b333d63dc2d3e4
LY-APV <d14bkru@gmail.com>
2026-04-09 18:31:18 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import add
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 6)
+        self.assertEqual(add(2, 3), 5)
 
 
 if __name__ == "__main__":
